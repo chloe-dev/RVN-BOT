ce93c2074be8f2d5ba509152bb5de1bc0d3b1fc8
chloe-dev <ChloeRBriggs@protonmail.com>
2024-12-01 03:37:48 -0500

    Implement command-line shutdown

--- src/main/java/bio/chloe/Main.java
+++ src/main/java/bio/chloe/Main.java
@@ -9,6 +9,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Scanner;
 
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
@@ -28,10 +30,56 @@ public class Main {
             JDA jdaObject = JDABuilder.createDefault(
                     configurationInstance.optString("botToken", null)
             ).build();
+
+            awaitShutdown(jdaObject);
+
+            System.exit(0); // TODO: Implement well-documented exit codes.
         } catch (InvalidTokenException invalidTokenException) {
             LOGGER.error("InvalidTokenException occurred whilst initializing JDA.");
 
             System.exit(-1); // TODO: Implement well-documented exit codes.
         }
+    }
+
+    private static void awaitShutdown(JDA jdaObject) {
+        Scanner terminalScanner = new Scanner(System.in);
+
+        while (terminalScanner.hasNextLine()) {
+            String terminalCommand = terminalScanner.nextLine();
+
+            if (terminalCommand.equalsIgnoreCase("shutdown")) {
+                shutdown(jdaObject);
+
+                break; // Once JDA has shut down, return to main().
+            } else {
+                LOGGER.error("Unknown terminal command entered, type \"shutdown\" to shutdown.");
+            }
+        }
+    }
+
+    private static void shutdown(JDA jdaObject) {
+        LOGGER.info("Initializing JDA graceful shutdown procedures.");
+
+        jdaObject.shutdown();
+
+        try {
+            if (!jdaObject.awaitShutdown(Duration.ofMinutes(5))) {
+                LOGGER.warn("JDA took more than five (5) minutes to shut down gracefully, forcing shutdown.");
+
+                jdaObject.shutdownNow();
+
+                if (jdaObject.awaitShutdown(Duration.ofMinutes(5))) {
+                    LOGGER.error("JDA took more than five (5) minutes to forcefully shut down. Terminating process.");
+
+                    System.exit(-1); // TODO: Implement well-documented exit codes.
+                }
+            }
+        } catch (InterruptedException threadInterruptedException) {
+            LOGGER.error("InterruptedException thrown whilst awaiting JDA shutdown (thread interruption).");
+
+            System.exit(-1); // TODO: Implement well-documented exit codes.
+        }
+
+        LOGGER.info("Graceful JDA shutdown successful, exiting the JVM.");
     }
 }
