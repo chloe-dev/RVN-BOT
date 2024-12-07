76ec2699330357241c74033cc8f956fb73f8b010
chloe-dev <ChloeRBriggs@protonmail.com>
2024-12-06 23:25:12 -0500

    Implement a rudimentary database manager

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,17 +20,23 @@ repositories {
 }
 
 dependencies {
+    // HikariCP: https://mvnrepository.com/artifact/com.zaxxer/HikariCP
+    implementation("com.zaxxer:HikariCP:6.2.1") // HikariCP 6.2.1 (18 November 2024).
+
     // JDA: https://mvnrepository.com/artifact/net.dv8tion/JDA
     implementation("net.dv8tion:JDA:5.2.1") // JDA 5.2.1 (10 November 2024).
 
     // JSON: https://mvnrepository.com/artifact/org.json/json.
     implementation("org.json:json:20240303") // JSON 20240303 (03 March 2024).
 
+    // Logback Classic: https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
+    implementation("ch.qos.logback:logback-classic:1.5.12") // Logback Classic 1.5.12 (25 October 2024).
+
     // SLF4J: https://mvnrepository.com/artifact/org.slf4j/slf4j-api
     implementation("org.slf4j:slf4j-api:2.0.16") // SLF4J 2.0.16 (10 August 2024).
 
-    // Logback Classic: https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
-    implementation("ch.qos.logback:logback-classic:1.5.12") // Logback Classic 1.5.12 (25 October 2024).
+    // SQLite JDBC: https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
+    implementation("org.xerial:sqlite-jdbc:3.47.1.0") // SQLite JDBC 3.47.1.0 (26 November 2024).
 
 }
 
---
+++ src/main/java/bio/chloe/managers/DatabaseManager.java
@@ -0,0 +1,145 @@
+package bio.chloe.managers;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseManager {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseManager.class);
+
+    private static HikariDataSource hikariDataSource;
+
+    private static volatile DatabaseManager instance;
+
+    private DatabaseManager(String dbUrl) {
+        initializeHikariDataSource(dbUrl);
+    }
+
+    private void initializeHikariDataSource(String dbUrl) {
+        HikariConfig hikariConfiguration = new HikariConfig();
+
+        hikariConfiguration.setJdbcUrl("jdbc:sqlite:" + dbUrl); // TODO: Surely this has to fail somehow... TestConnection method, perhaps?
+        hikariConfiguration.setDriverClassName("org.sqlite.JDBC");
+
+        // Connection pool settings.
+        hikariConfiguration.setMaximumPoolSize(10);
+        hikariConfiguration.setMinimumIdle(2);
+        hikariConfiguration.setIdleTimeout(300000);
+        hikariConfiguration.setConnectionTimeout(30000);
+
+        // SQLite properties.
+        Properties dataSourceProperties = new Properties();
+
+        dataSourceProperties.setProperty("pragma.foreign_keys", "ON");
+        dataSourceProperties.setProperty("pragma.journal_mode", "WAL");
+
+        hikariConfiguration.setDataSourceProperties(dataSourceProperties);
+
+        hikariDataSource = new HikariDataSource(hikariConfiguration);
+    }
+
+    public static DatabaseManager getInstance() {
+        return instance;
+    }
+
+    public static void initializeDatabaseManager(String dbUrl) {
+        if (instance == null) {
+            instance = new DatabaseManager(dbUrl);
+        }
+    }
+
+    public void shutdownDatabase() { // TODO: I'm not sure I like the idea of this being accessible via every DatabaseManager instance.
+        if (hikariDataSource != null && !(hikariDataSource.isClosed())) {
+            hikariDataSource.close();
+        }
+    }
+
+    // Generics for updating database information.
+
+    public void executeQuery(String sql, PreparedStatementConsumer preparedStatementConsumer, ResultSetConsumer resultSetConsumer) {
+        try (Connection dbConnection = hikariDataSource.getConnection(); PreparedStatement preparedStatement = dbConnection.prepareStatement(sql)) {
+            preparedStatementConsumer.accept(preparedStatement);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                resultSetConsumer.accept(resultSet);
+            }
+        } catch (SQLException sqlException) {
+            LOGGER.error("SQLException occurred whilst executing query \"{}.\"", sql);
+
+            throw new DatabaseException("SQLException occurred whilst executing a query!", sqlException);
+        }
+    }
+
+    public <T> T executeQueryWithResult(String sql, PreparedStatementConsumer preparedStatementConsumer, ResultSetMapper<T> resultSetMapper) {
+        try (Connection dbConnection = hikariDataSource.getConnection(); PreparedStatement preparedStatement = dbConnection.prepareStatement(sql)) {
+            preparedStatementConsumer.accept(preparedStatement);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return resultSetMapper.apply(resultSet);
+            }
+        } catch (SQLException sqlException) {
+            LOGGER.error("SQLException occurred whilst executing query \"{}.\"", sql);
+
+            throw new DatabaseException("SQLException occurred whilst executing a query!", sqlException);
+        }
+    }
+
+    public int executeUpdate(String sql, PreparedStatementConsumer preparedStatementConsumer) {
+        try (Connection dbConnection = hikariDataSource.getConnection(); PreparedStatement preparedStatement = dbConnection.prepareStatement(sql)) {
+            preparedStatementConsumer.accept(preparedStatement);
+
+            return preparedStatement.executeUpdate();
+        } catch (SQLException sqlException) {
+            LOGGER.error("SQLException occurred whilst executing query \"{}.\"", sql);
+
+            throw new DatabaseException("SQLException occurred whilst executing a query!", sqlException);
+        }
+    }
+
+    public int[] executeBatchUpdate(String sql, PreparedStatementConsumer preparedStatementConsumer, BatchPreparer batchPreparer) {
+        try (Connection dbConnection = hikariDataSource.getConnection(); PreparedStatement preparedStatement = dbConnection.prepareStatement(sql)) {
+            preparedStatementConsumer.accept(preparedStatement);
+
+            batchPreparer.accept(preparedStatement);
+
+            return preparedStatement.executeBatch();
+        } catch (SQLException sqlException) {
+            LOGGER.error("SQLException occurred whilst executing query \"{}.\"", sql);
+
+            throw new DatabaseException("SQLException occurred whilst executing a query!", sqlException);
+        }
+    }
+
+    @FunctionalInterface
+    public interface PreparedStatementConsumer {
+        void accept(PreparedStatement preparedStatement) throws SQLException;
+    }
+
+    @FunctionalInterface
+    public interface ResultSetConsumer {
+        void accept(ResultSet resultSet) throws SQLException;
+    }
+
+    @FunctionalInterface
+    public interface ResultSetMapper<T> {
+        T apply(ResultSet resultSet) throws SQLException;
+    }
+
+    @FunctionalInterface
+    public interface BatchPreparer {
+        void accept(PreparedStatement preparedStatement) throws SQLException;
+    }
+
+    public static class DatabaseException extends RuntimeException {
+        public DatabaseException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+}
